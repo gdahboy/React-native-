02e5e4ffda4c8d0c80b1e93910b9aefa795389d2
Maciot <abderrahim.gdah@karwisoft.com>
2018-07-17 09:16:23 +0100

    add Component

---
+++ Component/Questionthree.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import {View , Text } from 'react-native'
+
+export default class questionthree extends Component {
+  render() {
+    return (
+      <View style={{flex : 1 }}>
+        <Text>
+            question1
+            </Text>
+
+          </View>
+    )
+  }
+}
